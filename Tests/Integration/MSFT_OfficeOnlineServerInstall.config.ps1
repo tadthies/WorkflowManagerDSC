b6eb9743ae0e82a7520ed196ba1244f32f614ace
Nik Charlebois <nik_charlebois@hotmail.com>
2017-12-14 16:19:22 -0500

    Removed Integration Tests

--- Tests/Integration/MSFT_OfficeOnlineServerInstall.config.ps1
+++
@@ -1,8 +0,0 @@
-configuration MSFT_OfficeOnlineServerInstall_config {
-    Import-DscResource -ModuleName 'OfficeOnlineServerDsc'
-    node localhost {
-        OfficeOnlineServerInstall Integration_Test {
-            Path = 'C:\InstallBits\setup.exe'
-        }
-    }
-}
